964dbd109010908876c4d14cd05f72ef7609e3f1
issam el jaouhary <iseljao89@gmail.com>
2024-11-04 10:15:01 +0100

    fix: remove hono-mongodb-template

--- hono-mongodb-typescript/src/utils/types.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from "hono";
-import { IUser } from "../db";
-
-export type TDoceded = Pick<IUser, "_id" | "name" | "email">;
-
-export type ContextWithUser = Context<{
-  Variables: { user: TDoceded };
-}>;
