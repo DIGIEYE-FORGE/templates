2db2f291f81ac8f947110ee16ecac940504b33e4
oussamajamil <oussamajamil01@gmail.com>
2024-11-04 14:57:53 +0100

    fixed

--- nextjs-landing-page/src/app/components/upbar-mobile/index.tsx
+++ nextjs-landing-page/src/app/components/upbar-mobile/index.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import { twMerge } from "tailwind-merge";
 import { routes } from "../upbar-desktop/data";
 
@@ -26,6 +25,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 
 export function UpBarMobile({ className, ...props }: IUpbarProps) {
   return (
@@ -46,80 +46,82 @@ export function UpBarMobile({ className, ...props }: IUpbarProps) {
       />
       <Sheet>
         <SheetTrigger asChild>
-          <AlignJustify />
+          <Button variant={"ghost"} className="rounded-full uppercase">
+            <AlignJustify />
+          </Button>
         </SheetTrigger>
         <SheetContent className="w-full">
           <SheetHeader>
             <SheetTitle></SheetTitle>
             <SheetDescription></SheetDescription>
           </SheetHeader>
-          <div className="hide-scrollbar mb-2 flex h-[90%] w-full flex-col gap-4 overflow-y-auto px-4">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              width={100}
-              height={100}
-              className="rounded-lg object-cover"
-            />
-            <Accordion type="single" collapsible className="w-full">
-              {routes.map((route, index) => {
-                const { name, href, subRoutes } = route;
-                if (href) {
-                  return (
-                    <div
-                      key={index}
-                      className="flex h-[3rem] items-center border-b px-1 text-xl font-medium capitalize"
-                    >
-                      <Link href={href} legacyBehavior passHref>
-                        {name}
-                      </Link>
-                    </div>
-                  );
-                } else if (subRoutes) {
-                  return (
-                    <AccordionItem key={index} value={name}>
-                      <AccordionTrigger className="text-xl capitalize">
-                        {name}
-                      </AccordionTrigger>
-                      <AccordionContent className="grid grid-cols-2 gap-4 gap-y-6">
-                        {subRoutes.map((subRoute, index) => {
-                          const { name, image, description } = subRoute;
-                          return (
-                            <div
-                              key={index}
-                              className="relative flex flex-col space-y-1"
-                            >
-                              <Image
-                                src={image}
-                                alt={name}
-                                width={100}
-                                height={80}
-                                className="aspect-[1.2] w-full rounded-lg object-cover"
-                              />
+          <div className="hide-scrollbar mb-2 flex h-full w-full flex-col gap-4 px-4">
+            <div className="h-[94%] overflow-y-auto">
+              <Image
+                src="/logo.png"
+                alt="Logo"
+                width={100}
+                height={100}
+                className="rounded-lg object-cover"
+              />
+              <Accordion type="single" collapsible className="w-full">
+                {routes.map((route, index) => {
+                  const { name, href, subRoutes } = route;
+                  if (href) {
+                    return (
+                      <div
+                        key={index}
+                        className="flex h-[3rem] items-center border-b px-1 text-xl font-medium capitalize"
+                      >
+                        <Link href={href} legacyBehavior passHref>
+                          {name}
+                        </Link>
+                      </div>
+                    );
+                  } else if (subRoutes) {
+                    return (
+                      <AccordionItem key={index} value={name}>
+                        <AccordionTrigger className="text-xl capitalize">
+                          {name}
+                        </AccordionTrigger>
+                        <AccordionContent className="grid grid-cols-2 gap-4 gap-y-6">
+                          {subRoutes.map((subRoute, index) => {
+                            const { name, image, description } = subRoute;
+                            return (
+                              <div
+                                key={index}
+                                className="relative flex flex-col space-y-1"
+                              >
+                                <Image
+                                  src={image}
+                                  alt={name}
+                                  width={100}
+                                  height={80}
+                                  className="aspect-[1.2] w-full rounded-lg object-cover"
+                                />
 
-                              <h4 className="text-md font-semibold capitalize">
-                                {name}
-                              </h4>
-                              {description && (
-                                <p className="text-sm text-gray-500 first-letter:uppercase">
-                                  {description}
-                                </p>
-                              )}
-                            </div>
-                          );
-                        })}
-                      </AccordionContent>
-                    </AccordionItem>
-                  );
-                }
-              })}
-            </Accordion>
+                                <h4 className="text-md font-semibold capitalize">
+                                  {name}
+                                </h4>
+                                {description && (
+                                  <p className="text-sm text-gray-500 first-letter:uppercase">
+                                    {description}
+                                  </p>
+                                )}
+                              </div>
+                            );
+                          })}
+                        </AccordionContent>
+                      </AccordionItem>
+                    );
+                  }
+                })}
+              </Accordion>
+            </div>
+            <div className="flex justify-end">
+              <Button className="relative left-0 w-1/2">Sign In</Button>
+            </div>
           </div>
-          <SheetFooter>
-            <SheetClose asChild>
-              <Button className="rounded-full uppercase">sign In</Button>
-            </SheetClose>
-          </SheetFooter>
         </SheetContent>
       </Sheet>
     </header>
